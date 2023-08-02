dfe2c67be483111f7dd9e4dab112dc6c0ad30173
Handmetz <141221783+Handmetz@users.noreply.github.com>
2023-08-02 10:40:55 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+      <a class="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/HandMetz?ref_src=twsrc%5Etfw">Tweets by HandMetz</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         </p>
       </main>
 
