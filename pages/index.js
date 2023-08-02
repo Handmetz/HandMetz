22ca4a14b2dab5e0dad9b293bd42e84c521deae2
Handmetz <141221783+Handmetz@users.noreply.github.com>
2023-08-02 10:50:21 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
       </Head>
 
       <main>
-      <a> class="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/HandMetz?ref_src=twsrc%5Etfw">Tweets by HandMetz
+      <a class="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/HandMetz?ref_src=twsrc%5Etfw">Tweets by HandMetz
       </a> 
       <script> 
       <async src="https://platform.twitter.com/widgets.js" charset="utf-8">
