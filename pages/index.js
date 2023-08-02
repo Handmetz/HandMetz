67ac30341c4e2654a0f88c071cfc06457e19a299
Handmetz <141221783+Handmetz@users.noreply.github.com>
2023-08-02 10:35:58 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,21 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
+
 <a class="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/HandMetz?ref_src=twsrc%5Etfw">Tweets by HandMetz</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
