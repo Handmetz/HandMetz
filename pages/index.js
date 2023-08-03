2da5c58bebf1297b5785b800498153567200b648
Handmetz <141221783+Handmetz@users.noreply.github.com>
2023-08-03 14:14:55 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-      </Head>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mon site avec widget Twitter</title>
+</head>
+<body>
 
-      <main>
-      <a class="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/HandMetz?ref_src=twsrc%5Etfw">Tweets by HandMetz
-      </a> 
-      <script> 
-      <async src="https://platform.twitter.com/widgets.js" charset="utf-8">
-      </script>
-        </p>
-      </main>
+<a class="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/HandMetz?ref_src=twsrc%5Etfw">Tweets by HandMetz</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
-      <Footer />
-    </div>
-  )
-}
+</body>
+</html>
